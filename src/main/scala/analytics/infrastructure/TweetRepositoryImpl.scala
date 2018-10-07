b64efd22246d405115b6d75f15fb937197dfdbcf
Tsubaki <rockbouque@nifty.com>
2018-10-07 20:16:42 +0900

    feat: TweetRepositoryを追加

---
+++ src/main/scala/analytics/infrastructure/TweetRepositoryImpl.scala
@@ -0,0 +1,36 @@
+package analytics.infrastructure
+import java.text.SimpleDateFormat
+
+import analytics.model.{SessionTime, TweetRepository}
+import com.danielasfregola.twitter4s.entities.Tweet
+import scalikejdbc._
+import scalikejdbc.config.DBs
+
+import scala.concurrent.{ExecutionContext, Future}
+
+object TweetRepositoryImpl extends TweetRepository {
+
+  override def saveAll(sessionTime: SessionTime, tweets: Seq[Tweet])(implicit ec: ExecutionContext): Future[Unit] =
+    Future {
+
+      val records: Seq[Seq[Any]] = tweets.map { tweet =>
+        Seq(
+          sessionTime.value.toString,
+          tweet.id,
+          new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(tweet.created_at),
+          tweet.retweet_count,
+          tweet.favorite_count,
+          tweet.text
+        )
+      }
+
+      DBs.setupAll()
+
+      DB localTx { implicit session =>
+        sql"INSERT INTO tweet VALUES (?, ?, ?, ?, ?, ?)"
+          .batch(records: _*)
+          .apply()
+      }
+    }
+
+}
