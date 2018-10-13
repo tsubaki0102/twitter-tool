073267b7dcb6543a5a50fd2baf63f5d1af475d3e
Tsubaki <rockbouque@nifty.com>
2018-10-14 04:08:35 +0900

    fix: 日時のマッピングに失敗するのを修正

--- src/main/scala/analytics/infrastructure/TweetRepositoryImpl.scala
+++ src/main/scala/analytics/infrastructure/TweetRepositoryImpl.scala
@@ -1,5 +1,6 @@
 package analytics.infrastructure
-import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.time.{LocalDateTime, ZoneId}
 
 import analytics.model.{SessionTimeJST, TweetRepository}
 import com.danielasfregola.twitter4s.entities.Tweet
@@ -13,11 +14,13 @@ object TweetRepositoryImpl extends TweetRepository {
   override def saveAll(sessionTime: SessionTimeJST, tweets: Seq[Tweet])(implicit ec: ExecutionContext): Future[Unit] =
     Future {
 
+      val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
       val records: Seq[Seq[Any]] = tweets.map { tweet =>
         Seq(
-          sessionTime.value.toString,
+          sessionTime.value.format(formatter),
           tweet.id,
-          new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(tweet.created_at),
+          LocalDateTime.ofInstant(tweet.created_at.toInstant, ZoneId.of("Asia/Tokyo")),
           tweet.retweet_count,
           tweet.favorite_count,
           tweet.text
